fe66b4a90927ce8b3f8ec8457ba57182932fd052
Tom Hewitt <tomhwtt@gmail.com>
2025-04-19 10:42:05 -0400

    chore: add to_dict to User and write test

--- app/user.py
+++ app/user.py
@@ -7,5 +7,8 @@ class User:
         self.username = username
         self.email = email
 
+    def to_dict(self):
+        return {"id": self.id, "username": self.username, "email": self.email}
+
     def __str__(self):
         return f"User: {self.username} -> {self.id}"
