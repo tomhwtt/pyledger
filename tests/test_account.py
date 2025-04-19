4c5cf555c37aa766a5991a73266a31503b9a8bc0
Tom Hewitt <tomhwtt@gmail.com>
2025-04-19 12:30:27 -0400

    chore: implement deposit and write tests

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+import pytest
 from app.user import User
 from app.account import Account
 
@@ -14,3 +15,19 @@ def test_user_dict_returns_expected_values():
     assert account_dict["balance"] == 0
     assert isinstance(account_dict["created_at"], datetime.datetime)
     assert str(uuid_obj) == account_dict["id"]
+
+
+def test_balance_updates_as_expected():
+    user = User("tester", "tester@example.com")
+    account = Account(user.id)
+    account.deposit(100.00)
+
+    assert account.balance == 100.00
+
+
+def test_deposit_raises_value_error_if_amount_zero():
+    user = User("tester", "tester@example.com")
+    account = Account(user.id)
+
+    with pytest.raises(ValueError):
+        account.deposit(0)
