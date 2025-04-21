62a68b0c34b4b34a445fa2f36d46a6a3bcf47b33
Tom Hewitt <tomhwtt@gmail.com>
2025-04-21 17:14:26 -0400

    chore: calculate account balance

--- pyledger/services/account_service.py
+++ pyledger/services/account_service.py
@@ -1,12 +1,19 @@
 from uuid import UUID
+from decimal import Decimal
 from typing import List
 from pyledger.domain.account import Account
 from pyledger.repository.account_repository import AbstractAccountRepository
+from pyledger.repository.transaction_repository import AbstractTransactionRepository
 
 
 class AccountService:
-    def __init__(self, account_repository: AbstractAccountRepository):
+    def __init__(
+        self,
+        account_repository: AbstractAccountRepository,
+        transaction_repository: AbstractTransactionRepository,
+    ):
         self.repo = account_repository
+        self.transaction_repo = transaction_repository
 
     def create_account(self, owner_id: UUID) -> Account:
         account = Account.create(owner_id=owner_id)
@@ -18,3 +25,13 @@ class AccountService:
 
     def get_accounts_for_user(self, user_id: UUID) -> List[Account]:
         return self.repo.get_by_user_id(user_id)
+
+    def get_balance(self, account_id: UUID) -> Decimal:
+        transactions = self.transaction_repo.get_by_account_id(account_id)
+        balance = Decimal("0.00")
+        for txn in transactions:
+            if txn.type == "credit":
+                balance += Decimal(str(txn.amount))
+            elif txn.type == "debit":
+                balance -= Decimal(str(txn.amount))
+        return balance
